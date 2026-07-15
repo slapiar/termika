f66aa99b62760131a4548a681baa06f82df0b090
Slavomir Piar <33904511+slapiar@users.noreply.github.com>
2026-07-15 14:40:44 +0200

    Stabilizovať príchod z Prieskumníka a rozloženie ikon Analýzy

--- XC/analysis.php
+++ XC/analysis.php
@@ -5,11 +5,33 @@ ob_start();
 require __DIR__ . '/terrain-analysis-test.php';
 $html = (string)ob_get_clean();
 
+$headAssets = '';
+if (stripos($html, 'asset/workspace-cesium-toolbar-offset.css') === false) {
+    $headAssets .= '    <link rel="stylesheet" href="asset/workspace-cesium-toolbar-offset.css?v=20260715-02" data-workspace-cesium-toolbar-offset="true">' . "\n";
+}
+
+if ($headAssets !== '') {
+    $headEnd = stripos($html, '</head>');
+    if ($headEnd !== false) {
+        $html = substr_replace($html, $headAssets, $headEnd, 0);
+    }
+}
+
+$bodyScripts = '';
+if (stripos($html, 'js/workspace-cesium-toolbar-offset.js') === false) {
+    $bodyScripts .= '    <script src="js/workspace-cesium-toolbar-offset.js?v=20260715-02" data-workspace-cesium-toolbar-offset="true"></script>' . "\n";
+}
 if (stripos($html, 'js/explorer-analysis-bridge.js') === false) {
-    $script = '    <script src="js/explorer-analysis-bridge.js?v=20260715-02" data-explorer-analysis-bridge="true"></script>' . "\n";
+    $bodyScripts .= '    <script src="js/explorer-analysis-bridge.js?v=20260715-02" data-explorer-analysis-bridge="true"></script>' . "\n";
+}
+if (stripos($html, 'js/explorer-analysis-arrival.js') === false) {
+    $bodyScripts .= '    <script src="js/explorer-analysis-arrival.js?v=20260715-01" data-explorer-analysis-arrival="true"></script>' . "\n";
+}
+
+if ($bodyScripts !== '') {
     $bodyEnd = stripos($html, '</body>');
     if ($bodyEnd !== false) {
-        $html = substr_replace($html, $script, $bodyEnd, 0);
+        $html = substr_replace($html, $bodyScripts, $bodyEnd, 0);
     }
 }
 
