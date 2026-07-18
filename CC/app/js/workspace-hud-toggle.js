34102d223062c2ace7c4e5d2b93e37332bfe4684
Slavomir Piar <33904511+slapiar@users.noreply.github.com>
2026-07-18 09:46:30 +0200

    refactor: odstran globalny monolit z HUD proxy

--- CC/app/js/workspace-hud-toggle.js
+++ CC/app/js/workspace-hud-toggle.js
@@ -1,17 +1,14 @@
 /* CC host proxy. Implementácia patrí modulu camera-hud-toggle. */
 (() => {
   const currentScript = document.currentScript;
-  const globalUrl = new URL("global-map-tools.js?v=20260718-04", currentScript.src).href;
-  const globalScript = document.createElement('script');
-  globalScript.src = globalUrl;
-  globalScript.async = false;
-
   const moduleUrl = new URL("../../ux/camera-hud/camera-hud-toggle/source/XC__js__workspace-hud-toggle.js", currentScript.src).href;
+
   const moduleScript = document.createElement('script');
   moduleScript.src = moduleUrl;
   moduleScript.async = false;
-
-  const parent = currentScript?.parentNode || document.head;
-  parent.insertBefore(globalScript, currentScript?.nextSibling || null);
-  parent.insertBefore(moduleScript, globalScript.nextSibling);
+  if (currentScript && currentScript.parentNode) {
+    currentScript.parentNode.insertBefore(moduleScript, currentScript.nextSibling);
+  } else {
+    document.head.appendChild(moduleScript);
+  }
 })();
