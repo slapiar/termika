457440be65a66e42df7362e9449d16662df037fe
Slavomir Piar <33904511+slapiar@users.noreply.github.com>
2026-07-18 08:41:54 +0200

    Pripojiť globálne mapové nástroje cez LET bootstrap

--- CC/app/js/workspace-flight-toggle.js
+++ CC/app/js/workspace-flight-toggle.js
@@ -1,5 +1,18 @@
 /* CC host proxy. Implementácia patrí modulu flight-simulator-toggle. */
 (() => {
-  const moduleUrl = new URL("../../ux/flight-simulator/flight-simulator-toggle/source/XC__js__workspace-flight-toggle.js", document.currentScript.src).href;
-  document.write('<script src="' + moduleUrl.replaceAll('&', '&amp;').replaceAll('"', '&quot;') + '"><\/script>');
+  const currentScript = document.currentScript;
+  const globalUrl = new URL("global-map-tools.js?v=20260718-04", currentScript.src).href;
+  if (!document.querySelector('script[data-termika-global-map-tools]')) {
+    const globalScript = document.createElement('script');
+    globalScript.src = globalUrl;
+    globalScript.async = false;
+    globalScript.dataset.termikaGlobalMapTools = 'true';
+    (currentScript?.parentNode || document.head).insertBefore(globalScript, currentScript?.nextSibling || null);
+  }
+
+  const moduleUrl = new URL("../../ux/flight-simulator/flight-simulator-toggle/source/XC__js__workspace-flight-toggle.js", currentScript.src).href;
+  const moduleScript = document.createElement('script');
+  moduleScript.src = moduleUrl;
+  moduleScript.async = false;
+  (currentScript?.parentNode || document.head).insertBefore(moduleScript, currentScript?.nextSibling || null);
 })();
